8ac002309b509eb30eba766fabce5899fedb263b
Eric <ericbobcamargo@gmail.com>
2022-11-27 17:30:13 -0300

    build: implement prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.export = {
+  jsxSingleQuote: true,
+  singleQuote: true,
+  singleAttributePerLine: true,
+  proseWrap: "always",
+  semi: false,
+  trailingComma: "none",
+  tabWidth: 2,
+  printWidth: 120,
+  useEditorConfig: true,
+};
